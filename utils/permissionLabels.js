0e175760a600fa9177bf1ee966af23b4ea9a59e0
nhat2109 <dangnhat20031010@gmail.com>
2024-11-06 13:47:07 +0700

    them authorization cho them sua xoa cac muc

--- utils/permissionLabels.js
+++ utils/permissionLabels.js
@@ -1,19 +1,23 @@
 // config/permissionLabels.js
 
 const permissionLabels = {
-    "products-category_view": "Xem",
-    "products-category_create": "Thêm mới",
-    "products-category_edit": "Chỉnh sửa",
-    "products-category_delete": "Xóa",
-    "products_view": "Xem",
-    "products_create": "Thêm mới",
-    "products_edit": "Chỉnh sửa",
-    "products_delete": "Xóa",
-    "roles_view": "Xem",
-    "roles_create": "Thêm mới",
-    "roles_edit": "Chỉnh sửa",
-    "roles_delete": "Xóa",
-    "roles_permissions": "Phân quyền"
+    "products-category_view": "Xem danh mục sản phẩm",
+    "products-category_create": "Thêm mới danh mục sản phẩm",
+    "products-category_edit": "Chỉnh sửa danh mục sản phẩm",
+    "products-category_delete": "Xóa danh mục sản phẩm",
+    "products_view": "Xem sản phẩm",
+    "products_create": "Thêm mới sản phẩm",
+    "products_edit": "Chỉnh sửa sản phẩm",
+    "products_delete": "Xóa sản phẩm",
+    "roles_view": "Xem nhóm quyền",
+    "roles_create": "Thêm mới nhóm quyền",
+    "roles_edit": "Chỉnh sửa nhóm quyền",
+    "roles_delete": "Xóa nhóm quyền",
+    "roles_permissions": "Phân quyền",
+    "accounts_view": "Xem danh sách tài khoản",
+    "accounts_create": "Thêm mới danh sách tài khoản",
+    "accounts_edit": "Chỉnh sửa danh sách tài khoản",
+    "accounts_delete": "Xóa danh sách tài khoản",
 };
 
 module.exports = permissionLabels;
